9df6b61725b6c31813ed4b527e8f342d2099d51e
Matheus Reynaud Soares <matheuxreynaud@gmail.com>
2024-08-21 00:41:53 -0300

    zero-to-hero

---
+++ 01-Fundamentals-Part-1/assignments/assignments.js
@@ -0,0 +1,12 @@
+/*
+Assignment 01
+
+let country = "Brazil";
+let continent = "South America";
+let population = "215.300.000";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+*/
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Matheus");
+console.log(23);
+
+let firstName = "Matthew";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let matheus_matthew = "MATT";
+let $function = 30;
+
+let person = "Matheus";
+let PI = 3.1415;
+
+//This is better...
+let myFirstJob = 'Server';
+let myCurrentJob = 'Programmer';
+
+//Than this
+let job1 = 'Server';
+let job2 = 'Programmer';
+
+console.log(myCurrentJob);
